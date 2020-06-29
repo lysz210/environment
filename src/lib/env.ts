30ea6db7b6666bdd2ee0d12da3059b43f05ce0b1
Lingyong Sun <lysz210@gmail.com>
2020-06-29 23:47:42 +0200

    update env function with validation

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,14 @@
-export const env = (key: string): any => {
+import { InvalidArgumentError, UndefinedEnvError } from "./exceptions";
+import { isEmptyString } from "src/helpers";
+import { isUndefined } from "util";
+
+export function env(key: string): string {
+    if (isEmptyString(key)) {
+        throw new InvalidArgumentError('The key cannot be empty!')
+    }
+    const retVal = process.env[key];
+    if (isUndefined(retVal)) {
+        throw new UndefinedEnvError(`Undefined env['${key}']`)
+    }
     return process.env[key];
 }
