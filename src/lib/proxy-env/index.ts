4cce9b4cd314dc065fe2e43cc98da7a16ea6b53d
Lingyong Sun <lysz210@gmail.com>
2020-06-30 20:45:57 +0200

    add smartEnv with Proxy
    convert with this lib converters mapped with the return type

--- src/lib/proxy-env/index.ts
+++ src/lib/proxy-env/index.ts
@@ -1,21 +1,30 @@
 import { env } from "../env";
 import { asArray } from "../array";
-
+import { asJson } from "../json";
+import { asNumber } from "../number";
+import { asBoolean } from "../boolean";
 
 type EnvValue = string | number | boolean | string[] | object;
-
 
 export function createSmart (map: {[key: string]: string}) : EnvValue {
     return new Proxy(env, {
         get (_, key: string) {
             const converterName = map[key] || 'string';
             switch (converterName) {
+                case 'boolean':
+                    return asBoolean(key);
+                case 'number':
+                    return asNumber(key);
+                case 'json':
+                    // falls through
+                case 'object':
+                    return asJson(key);
                 case 'array':
-                    return asArray(key)
+                    return asArray(key);
                 case 'string':
                     // falls through
                 default:
-                    return env[key]
+                    return env(key);
             }
         } 
     })
